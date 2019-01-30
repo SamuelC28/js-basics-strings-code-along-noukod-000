79d637c6d978197f9ff31f17c55d9866dc63be4c
Samuel Cornet <samuel@noukod.com>
2019-01-30 14:15:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 let currentUser = "Samuel";
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
-// const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
 const excitedWelcomeMessage = "${welcomeMessage}.toUpperCase() + ${currentUser}!";
 
 const shortGreeting = 'Welcome, ${currentUser}';
