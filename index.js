6b899f6da1483eaca59f0707f0126cdb27bbcefb
Samuel Cornet <samuel@noukod.com>
2019-01-30 14:14:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 let currentUser = "Samuel";
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 // const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
-const excitedWelcomeMessage = "welcomeMessage.toUpperCase() + ${currentUser}!";
+const excitedWelcomeMessage = "${welcomeMessage}.toUpperCase() + ${currentUser}!";
 
 const shortGreeting = 'Welcome, ${currentUser}';
